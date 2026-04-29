32a36522321851d0219fd89f205f6a193f255dce
tom <tpfitz42@gmail.com>
2026-04-29 17:57:30 -0600

    fixing header and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tom-fitz.github.io',
-	base: 'knights-wood-works',
+	base: '/knights-wood-works',
 });
