b52a1a10cb2e3da2f248a2b970a7231a13bf1c45
tom <tpfitz42@gmail.com>
2026-04-29 17:46:42 -0600

    adding in gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://tom-fitz.github.io',
+	base: 'knights-wood-works',
+});
